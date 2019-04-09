8f852d392ef9c7e57d1bd29032d278751b0bc273
mchengye93 <marbochengye@gmail.com>
2019-04-09 10:46:08 -0700

    part 1 under

--- src/underbar.js
+++ src/underbar.js
@@ -44,9 +44,7 @@
     
     if (n > 0) {
       return array.slice(-n);
-    }
-
-    else {
+    } else {
       return [];
     }
   
@@ -58,6 +56,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)){
+      for(var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for(var key in collection){
+        iterator(collection[key], key, collection);
+      }  
+    }
+  
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -79,6 +87,14 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var array = [];
+    _.each(collection, function(value){
+      if(test(value)){
+        array.push(value);  
+      }
+    });
+    
+    return array;
   };
 
   // Return all elements of an array that don't pass a truth test.
